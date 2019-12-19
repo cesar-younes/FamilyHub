6a5a59ee38e52842267df41cc3ec20a9a5346e55
Cesar Younes <cesar.younes@outlook.com>
2019-12-20 01:12:18 +0400

    dotnet core 3.0 implementation with Face SDK

---
+++ src/Controllers/FaceServiceController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FamilyHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FaceServiceController : ControllerBase
+    {
+        private readonly ILogger<FaceServiceController> _logger;
+
+        public FaceServiceController(ILogger<FaceServiceController> logger)
+        {
+            logger = _logger;
+        }
+
+        // GET: api/FaceService
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/FaceService/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/FaceService
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/FaceService/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Services/CognitiveServicesFace.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+namespace FamilyHub.Services
+{
+    public class CognitiveServicesFace
+    {
+        private static string FaceServiceKey = Environment.GetEnvironmentVariable("FACESERVICEKEY");
+        private static string FaceServiceRegion = Environment.GetEnvironmentVariable("FACESERVICEREGION");
+        private static readonly IFaceClient _faceServiceClient = new FaceClient(
+            new ApiKeyServiceClientCredentials(FaceServiceKey),
+            new System.Net.Http.DelegatingHandler[] { });
+
+        public static async Task CreatePersonGroupIfNotExistsAsync(string largePersonGroupId, string personGroupName)
+        {
+            try
+            {
+                var largePersonGroup = await _faceServiceClient.LargePersonGroup.GetAsync(largePersonGroupId);
+
+                if (largePersonGroup == null)
+                {
+                    await _faceServiceClient.LargePersonGroup.CreateAsync(largePersonGroupId, name: personGroupName);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("" + e);
+                throw;
+            }
+        }
+
+        public static async Task AddEmptyPersonInGroupAsync(string personName, string largePersonGroupId)
+        {
+            try
+            {
+                Person person = await _faceServiceClient.LargePersonGroupPerson.CreateAsync(
+                    // Id of the PersonGroup that the person belongs to
+                    largePersonGroupId,
+                    // Name of the person
+                    personName
+                );
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("" + e);
+                throw;
+            }
+
+        }
+
+        public static async Task AddFaceToPersonAsync(Guid personId, string imageUrl, string largePersonGroupId)
+        {
+            try
+            {
+                await _faceServiceClient.LargePersonGroupPerson.AddFaceFromUrlAsync(largePersonGroupId, personId, imageUrl);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("" + e);
+                throw;
+            }
+        }
+
+        public static async Task TrainPersonGroupAsync(string largePersonGroupId)
+        {
+            try
+            {
+                await _faceServiceClient.LargePersonGroup.TrainAsync(largePersonGroupId);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("" + e);
+                throw;
+            }
+        }
+
+        public static async Task<bool> ReturnTrainingStatusWhenTrueAsync(string largePersonGroupId)
+        {
+            TrainingStatus trainingStatus = null;
+            while (true)
+            {
+                trainingStatus = await _faceServiceClient.LargePersonGroup.GetTrainingStatusAsync(largePersonGroupId);
+
+                if (trainingStatus.Status != TrainingStatusType.Running)
+                {
+                    break;
+                }
+
+                await Task.Delay(1000);
+            }
+
+            return true;
+        }
+
+        public static async Task<Person> DetectPersonAsync(string imageUrl, string largePersonGroupId)
+        {
+           
+            var faces = await _faceServiceClient.Face.DetectWithUrlAsync(imageUrl);
+            var faceIds = faces.Select(face => face.FaceId.Value).ToArray();
+
+            var results = await _faceServiceClient.Face.IdentifyAsync(faceIds, largePersonGroupId:largePersonGroupId);
+            foreach (var identifyResult in results)
+            {
+                //Console.WriteLine("Result of face: {0}", identifyResult.FaceId);
+                if (identifyResult.Candidates.Count == 0)
+                {
+                    Console.WriteLine("No one identified");
+                    return null;
+                }
+                else
+                {
+                    // Get top 1 among all candidates returned
+                    var candidateId = identifyResult.Candidates[0].PersonId;
+                    var person = await _faceServiceClient.LargePersonGroupPerson.GetAsync(largePersonGroupId, candidateId);
+                    Console.WriteLine("Identified as {0}", person.Name);
+                    return person;
+                }
+            }
+
+            return null;
+        }
+    }
+}
